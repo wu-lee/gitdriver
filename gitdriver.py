c6ad816cb1a3556e25d52afea46f06be738f804e
David Fraser <davidf@j5int.com>
2016-06-09 18:12:57 +0200

    Support exporting multiple formats, including all the available formats, and use mimetypes to guess the extensions Also, name the file itself with the title of the document

--- gitdriver.py
+++ gitdriver.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import argparse
+import mimetypes
 import subprocess
 import yaml
 
@@ -11,52 +12,63 @@ from drive import GoogleDrive, DRIVE_RW_SCOPE
 def parse_args():
     p = argparse.ArgumentParser()
     p.add_argument('--config', '-f', default='gd.conf')
-    p.add_argument('--text', '-T', action='store_const', const='text/plain',
+    p.add_argument('--text', '-T', action='append_const', const='text/plain',
             dest='mime_type')
-    p.add_argument('--html', '-H', action='store_const', const='text/html',
+    p.add_argument('--html', '-H', action='append_const', const='text/html',
             dest='mime_type')
-    p.add_argument('--mime-type', dest='mime_type')
+    p.add_argument('--mime-type', action='append', dest='mime_type', default=[])
+    p.add_argument('--all-types', action='store_true',
+            help='Export all available mime types')
     p.add_argument('--raw', '-R', action='store_true',
             help='Download original document if possible.')
     p.add_argument('docid')
 
     return p.parse_args()
 
-def commit_revision(gd, opts, rev):
-    with open('content', 'w') as fd:
-        if 'exportLinks' in rev and not opts.raw:
-            # If the file provides an 'exportLinks' dictionary,
-            # download the requested MIME type.
-            r = gd.session.get(rev['exportLinks'][opts.mime_type])
-        elif 'downloadUrl' in rev:
-            # Otherwise, if there is a downloadUrl, use that.
-            r = gd.session.get(rev['downloadUrl'])
-        else:
-            raise KeyError('unable to download revision')
+# override text/plain as otherwise it returns .ksh
+MIME_TYPE_SUFFIXES = {
+    'text/plain': '.txt',
+}
 
-        # Write file content into local file.
-        for chunk in r.iter_content():
-            fd.write(chunk)
+def commit_revision(gd, opts, rev, basename='content'):
+    mime_types = rev['exportLinks'].keys() if opts.all_types else opts.mime_type
+    for mime_type in mime_types:
+        filename_suffix = MIME_TYPE_SUFFIXES.get(mime_type, mimetypes.guess_extension(mime_type, False))
+        filename_suffix = filename_suffix or ".%s" % mime_type.replace('/', '_')
+        filename = '%s%s' % (basename, filename_suffix)
+        with open(filename, 'w') as fd:
+            if 'exportLinks' in rev and not opts.raw:
+                # If the file provides an 'exportLinks' dictionary,
+                # download the requested MIME type.
+                r = gd.session.get(rev['exportLinks'][mime_type])
+            elif 'downloadUrl' in rev:
+                # Otherwise, if there is a downloadUrl, use that.
+                r = gd.session.get(rev['downloadUrl'])
+            else:
+                raise KeyError('unable to download revision')
 
-    # Prepare environment variables to change commit time
-    env = os.environ.copy()
-    env['GIT_COMMITTER_DATE'] = rev['modifiedDate']
-    env['GIT_AUTHOR_DATE'] = rev['modifiedDate']
-    env['GIT_COMMITTER_NAME'] = rev['lastModifyingUserName']
-    env['GIT_AUTHOR_NAME'] = rev['lastModifyingUserName']
-    env['GIT_COMMITTER_EMAIL'] = rev['lastModifyingUser']['emailAddress']
-    env['GIT_AUTHOR_EMAIL'] = rev['lastModifyingUser']['emailAddress']
+            # Write file content into local file.
+            for chunk in r.iter_content():
+                fd.write(chunk)
 
-    # Commit changes to repository.
-    subprocess.call(['git', 'add', 'content'])
-    subprocess.call(['git', 'commit', '-m',
-        'revision from %s' % rev['modifiedDate']], env=env)
+        # Prepare environment variables to change commit time
+        env = os.environ.copy()
+        env['GIT_COMMITTER_DATE'] = rev['modifiedDate']
+        env['GIT_AUTHOR_DATE'] = rev['modifiedDate']
+        env['GIT_COMMITTER_NAME'] = rev['lastModifyingUserName']
+        env['GIT_AUTHOR_NAME'] = rev['lastModifyingUserName']
+        env['GIT_COMMITTER_EMAIL'] = rev['lastModifyingUser']['emailAddress']
+        env['GIT_AUTHOR_EMAIL'] = rev['lastModifyingUser']['emailAddress']
+
+        # Commit changes to repository.
+        subprocess.call(['git', 'add', filename])
+    subprocess.call(['git', 'commit', '-m', 'revision from %s' % rev['modifiedDate']], env=env)
 
 
 def main():
     opts = parse_args()
-    if not opts.mime_type:
-		print "Exactly one mime-type must be given!"
+    if not opts.mime_type and not opts.all_types:
+		print "At least one mime-type must be given!"
 		exit(1)
     cfg = yaml.load(open(opts.config))
     gd = GoogleDrive(
@@ -82,7 +94,7 @@ def main():
         for rev in gd.revisions(opts.docid):            
             if revision_matched:
                 print "New revision: " + rev['modifiedDate']
-                commit_revision(gd, opts, rev)
+                commit_revision(gd, opts, rev, md['title'])
             if rev['modifiedDate'] in last_commit_message:
                 print "Found matching revision: " + rev['modifiedDate']
                 revision_matched = True
@@ -95,7 +107,7 @@ def main():
 
         # Iterate over the revisions (from oldest to newest).
         for rev in gd.revisions(opts.docid):
-            commit_revision(gd, opts, rev)
+            commit_revision(gd, opts, rev, md['title'])
 if __name__ == '__main__':
     main()
 
