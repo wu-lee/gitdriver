93730337c1b5a1a6fad6e59b86c820eba6d6ac1a
David Fraser <davidf@j5int.com>
2017-10-13 11:44:40 +0200

    Handle files with / in it

--- gitdriver.py
+++ gitdriver.py
@@ -39,7 +39,7 @@ def commit_revision(gd, opts, rev, md):
     # Prepare environment variables to change commit time
     env = os.environ.copy()
     date = rev['modifiedDate']
-    basename = md.get('title', 'content')
+    basename = md.get('title', 'content').replace('/', '_')
     owner_users = [owner.get('displayName', None) for owner in md.get('owners', [])]
     owner_emails = [owner.get('emailAddress', None) for owner in md.get('owners', [])]
     user = rev.get('lastModifyingUserName', None)
